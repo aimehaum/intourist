3df15218bf12ea86382dd7bb139f8308f27cc07d
aimehaum <aimehaum@gmail.com>
2021-08-12 17:16:20 +0600

    added class FeedbackAdmin

--- places/admin.py
+++ places/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from .models import Place, Feedback
 
 admin.site.register(Place)
-admin.site.register(Feedback)
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['text', 'place', 'user', 'checked']
+    list_editable = ['checked']
+    list_filter = ['checked']
+    search_fields = ['text', 'place__name', 'place__location','place__description']
+
+    fields = ['user', 'place', 'text']
+    readonly_fields = ['place', 'text']
+
+admin.site.register(Feedback, FeedbackAdmin)
 
 
 # Register your models here.
